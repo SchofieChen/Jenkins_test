33768de431218d07b2769dcbd9401377f54740d5
SchofieChen <SchofieChen>
2020-07-30 23:47:43 +0800

    first commit

---
+++ Jenkins_Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenkins_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helloworld();
+        }
+
+        static void Helloworld()
+        {
+            Console.WriteLine("Hello it is Jenkins test");
+        }
+    }
+
+    
+}
